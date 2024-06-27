e589372dfd1ad41f87ffe6fa82e1a19409cafe24
Praneetha Ravi <pravi2@albany.edu>
2024-06-27 12:26:26 -0400

    Book Service

---
+++ Frontend/src/app/services/book.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  private apiUrl = 'http://localhost:5120/api/book';
+
+  constructor(private http : HttpClient) { }
+
+  
+  getAllBooks(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  getBestSellers(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl+'/GetBestSellers');
+  }
+
+  getBookById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+
+}
